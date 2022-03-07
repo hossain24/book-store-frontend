c646bd1e441f0d9dfcaf37a175156f0f6504c6ca
hossain24 <haj.ctg@gmail.com>
2022-03-07 00:02:03 +0000

    dashboard update

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import BookStore from './root/BookStore';
 import SignIn from './components/Authentication/SignIn/SignIn';
-import SignUp from './components/Authentication/SignIn/SignIn';
-import DashBoard from './components/PrivatePage/DashBoard/DashBoard';
-import OrderList from './components/PrivatePage/OrderList/OrderList';
-import Private from './components/PrivatePage/Private/Private';
-
+import Dashboard from './components/PrivatePage/DashBoard/DashBoard';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<BookStore />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="private" element={<Private />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="dashboard/*" element={<DashBoard />} />
-        <Route path="order-list" element={<OrderList />} />
+        <Route path='/' element={<BookStore />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </>
   );
